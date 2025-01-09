22f3ea1a30f62eb335ceb0a9c00e7fb662f1e704
Nicolas Ferro <nicolasalexandrinoferro@gmail.com>
2025-01-09 12:48:33 -0300

    começando com a classe

---
+++ product-backend/src/main/java/com/nicolas/product_backend/resources_or_controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.nicolas.product_backend.resources_or_controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class ProductController {
+
+    @GetMapping("product")
+    public String getProduct() {
+        return ("Eu sou um produto");
+    }
+
+}
